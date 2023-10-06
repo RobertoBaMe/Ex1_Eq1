3809bfa0a14f461e4208595ae540dd810533c88d
EdwardRG69 <edwardrg020@gmail.com>
2023-10-05 23:21:29 -0600

    Programación Win
    Falta el arte que alguien se rife XD

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,12 +8,25 @@ public class Score : MonoBehaviour
     private float puntos;
     //controla el componente texto
     private TextMeshProUGUI textMesh;
+    //Variable para el panel de Win
+    [SerializeField] private GameObject panelWin; 
+
     private void Start()
     {
         textMesh = GetComponent<TextMeshProUGUI>();
+        //Siempre que se inicie el juego el panel iniciara apagado 
+        panelWin.SetActive(false);
+        Time.timeScale = 1;
+        
     }
     private void Update()
     {
+        //Si los puntos son mayor o igual a 100 se activa el panel de win y se pausa el juego 
+        if(puntos >= 100)
+        {
+            panelWin.SetActive(true);
+            Time.timeScale = 0;
+        }
         //Cobierte los puntos a String para que pueda salir en pantalla
         textMesh.text = puntos.ToString("0");
     }
